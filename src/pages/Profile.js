589f0c5dad8574f08ea2707a6b08a992580c2123
Gülbahar Erol <ggulbaharerol@gmail.com>
2022-09-24 01:51:04 +0300

    theme update

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,20 +1,22 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Dimensions } from "react-native";
 import Header from "../components/header";
 import ProfilePhoto from "../components/ProfilePhoto";
 import Button from "../components/button";
 import { useNavigation } from "@react-navigation/native";
-import {useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
+
+const screenHeight = Dimensions.get("screen").height;
 const Profile = () => {
   const { navigate } = useNavigation();
-  
+  const theme = useSelector((state) => state.theme.activeTheme);
   return (
-    <View>
+    <View style={{ backgroundColor: theme.backgroundColor }}>
       <Header title="Profile" />
       <ProfilePhoto URL={"https://picsum.photos/200/200"} />
       <View style={styles.buttonContainer}>
-        <Button placeholder="Setting" onPress={()=>navigate('Edit')}/>
-        <Button placeholder="Theme" onPress={()=>navigate('Theme')}/>
+        <Button placeholder="Setting" onPress={() => navigate("Edit")} />
+        <Button placeholder="Theme" onPress={() => navigate("Theme")} />
         <Button placeholder="Log Out" />
       </View>
     </View>
@@ -25,6 +27,7 @@ export default Profile;
 
 const styles = StyleSheet.create({
   buttonContainer: {
+    height: screenHeight,
     alignItems: "center",
     marginTop: 40,
   },
--- src/pages/SharePhoto.js
+++ src/pages/SharePhoto.js
@@ -1,14 +1,17 @@
 import React ,{useState} from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet,Dimensions } from "react-native";
 import Header from "../components/header";
 import Button from "../components/button";
 import { useNavigation } from "@react-navigation/native";
 import * as ImagePicker from 'expo-image-picker';
+import { useSelector } from "react-redux";
 
+const screenHeight = Dimensions.get("screen").height;
 
 const SharePhoto = () => {
   const {navigate} = useNavigation();
   const [image, setImage] = useState(null);
+  const theme = useSelector((state) => state.theme.activeTheme);
 
   const pickImage = async () => {
     // No permissions request is necessary for launching the image library
@@ -29,7 +32,7 @@ const SharePhoto = () => {
   };
 
   return (
-    <View>
+    <View style={{ backgroundColor: theme.backgroundColor} }>
       <Header title="Share Photo" />
       <View style={styles.container}>
         <Button
@@ -47,5 +50,6 @@ export default SharePhoto;
 const styles = StyleSheet.create({
   container: {
     alignItems: "center",
+    height: screenHeight,
   },
 });
