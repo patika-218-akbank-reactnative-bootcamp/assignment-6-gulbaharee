551f03c2f46fcd829420b9808411a2dfd58a816d
Gülbahar Erol <ggulbaharerol@gmail.com>
2022-09-24 01:57:33 +0300

    edit page added

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,12 +1,35 @@
 import React from "react";
-import { View,Text } from "react-native";
+import { View, StyleSheet,Dimensions } from "react-native";
+import Button from "../components/button";
+import Header from "../components/header";
+import Input from "../components/input";
+import Profile from "./Profile";
+import {useSelector,useDispatch} from 'react-redux';
+import { useNavigation } from "@react-navigation/native";
+const screenHeight= Dimensions.get('screen').height;
 
-const Edit=()=>{
-    return(
-        <View>
-            <Text>Edit</Text>
-        </View>
-    )
-}
+const Edit = () => {
+    const { goBack } = useNavigation();
+    const theme = useSelector(state=>state.theme.activeTheme);
+  return (
+    <View>
+      <Header title="Edit" goback={true} onPress={() => goBack("Profile")} />
+      <View
+        style={[styles.container, { backgroundColor: theme.backgroundColor }]}
+      >
+        <Input placeholder='username'/>
+        <Input placeholder='email'/>
+        <Input placeholder='password'/>
+        <Button placeholder='Save'/>
+      </View>
+    </View>
+  );
+};
 
 export default Edit;
+const styles=StyleSheet.create({
+    container:{
+        alignItems:'center',
+        height:screenHeight,
+    }
+})
